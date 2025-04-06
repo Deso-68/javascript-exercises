4e1508564a12544a4d027c7e63121db678cca5e4
Deso-68 <174117432+Deso-68@users.noreply.github.com.>
2025-04-06 13:35:20 +0800

    Done solving 02 exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+    let number = 0;
+    let string = "";
+    const WORD = "hey";
 
+const repeatString = function(word, number) {
+    
+    for(let i = 0; i < number; ++i) {
+        string += word;
+    }
+    return string;
 };
 
 // Do not edit below this line
