c20492a790d3cd88d2e4053b52501d1f0ba50846
Deso-68 <174117432+Deso-68@users.noreply.github.com.>
2025-04-06 22:35:34 +0800

    Done solving 04 exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,27 @@
-const removeFromArray = function() {
+const removeFromArray = function(...testArray) {
+    let fromArray = testArray[0];
+    
+    let newArray = fromArray.filter(removeNumber, testArray);
+
+
+    let temp = newArray;
+    return newArray;
 };
+
+removeFromArray([1, 2, 3, 4], 3, 2);
+
+function removeNumber(number) {
+    for (let i = 1; i < this.length; i++) {
+        if(number !== this[i]) {
+            continue;
+        }
+        else if(number === this[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
