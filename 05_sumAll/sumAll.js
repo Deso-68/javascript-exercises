1ac36b66153647f85a949d2db243fe9b1e80793c
Deso-68 <174117432+Deso-68@users.noreply.github.com.>
2025-04-07 00:05:29 +0800

    Done solving 05 exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(sumStart, sumEnd ) {
+    let sumAll = 0;
 
+    if(sumStart > sumEnd) {
+        [sumStart, sumEnd] = [sumEnd , sumStart];
+    }
+    
+    if(!Number.isInteger(sumStart) || !Number.isInteger(sumEnd) || 
+      (sumEnd < 0 || sumStart < 0)) {
+        return "ERROR";
+    }
+
+    for ( ;sumStart <= sumEnd; sumStart++) {
+         sumAll += sumStart;        
+    }
+    return sumAll;
 };
 
+sumAll(2.5, 4);
 // Do not edit below this line
 module.exports = sumAll;
