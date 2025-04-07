c33a3285fb5e1f4e97e4dc738ab61e6e9a610c34
Deso-68 <174117432+Deso-68@users.noreply.github.com.>
2025-04-07 19:38:14 +0800

    Done solving 07 exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const convertToCelsius = function() {
-};
+// Applies standard fahrenheit to celcius conversion
+const convertToCelsius = temperature => 
+(+Number.parseFloat((temperature - 32) * 5 / 9).toFixed(1));
 
-const convertToFahrenheit = function() {
-};
+// Applies standard celcius to fahrenheit conversion
+const convertToFahrenheit = temperature => 
+(+Number.parseFloat(((temperature * 9 / 5) + 32).toFixed(1)));
 
+convertToCelsius(100);
+
+let i = 0;
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
